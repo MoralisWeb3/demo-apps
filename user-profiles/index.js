20c7a6a331d7a7eb6ed9756736bd606acc427819
Jeremy May <may.jeremy.r@gmail.com>
2021-03-16 11:04:47 -0700

    set email and password in profile

--- user-profiles/index.js
+++ user-profiles/index.js
@@ -39,6 +39,7 @@ async function loginWithEmail(isSignUp) {
 
     render();
   } catch (error) {
+    console.log(error);
     alert("invalid username or password");
   }
 }
@@ -51,8 +52,6 @@ function renderHeader() {
       <button id="btn-logout">Logout</button>
     `;
     document.getElementById("btn-logout").onclick = logOut;
-
-    renderProfile(user);
   } else {
     // show the login button if user not logged in
     appHeaderContainer.innerHTML = `
@@ -65,8 +64,6 @@ function renderHeader() {
     document.getElementById("btn-signup").onclick = function () {
       renderLogin(true);
     };
-
-    renderWelcome();
   }
 }
 
@@ -85,7 +82,7 @@ function buildLoginComponent(isSignUp = false) {
       <hr/>
       <form id="frm-login">
         <div>
-          <label for="email">Email</label>
+          <label for="email">Email/Username</label>
           <input type="text" id="email" name="email"/>
         </div>
         <div>
@@ -98,33 +95,65 @@ function buildLoginComponent(isSignUp = false) {
   `;
 }
 
+function getAddressTxt(address) {
+  return `${address.substr(0, 4)}...${address.substr(
+    address.length - 4,
+    address.length
+  )}`;
+}
+
 function buildProfileComponent(user) {
+  // show default avatar if not defined
+  const imgUrl = user.attributes.avatar
+    ? user.attributes.avatar.url()
+    : "default-user.png";
+  console.log("buildProfileComponent:: imgUrl:", imgUrl);
+
+  // construct list of addresses
+  let addressList = "<p>None</p>";
+  if (user.attributes.accounts) {
+    addressList = user.attributes.accounts
+      .map(function (account) {
+        return `<li>${getAddressTxt(account)}</li>`;
+      })
+      .join("");
+  }
+
   return `
     <div class="container">
-      <form action="">
+      <form action="" id="frm-profile">
         <div>
           <span>Avatar</span>
           <img
             class="user-img"
-            src="https://i1.wp.com/fabiusmaximus.com/wp-content/uploads/2013/09/20130910-homer-simpson.jpg"
+            src="${imgUrl}"
             alt="Homer"
           />
+          <button id="btn-change-avatar">Change</button>
         </div>
         <div>
-          <label for="name">Name</label>
-          <input type="text" id="name" />
+          <label for="name">User Name</label>
+          <input type="text" id="name" value="${
+            user.attributes.username || ""
+          }"/>
         </div>
         <div>
           <label for="email">Email</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" value="${
+            user.attributes.email || ""
+          }"/>
+        </div>
+        <div id="profile-set-pass">
+          <p>Setting a password allows login via email</p>
+          <button id="btn-profile-set-pass">Set Password</button>
         </div>
         <div>
           <h3>Addresses</h3>
           <ul>
-            <li>0xda6638b92C762A7ea228b8B66B209E64544ccB83</li>
-            <li>0xda6638b92C762A7ea228b8B66B209E64544ccB83</li>
+            ${addressList}
           </ul>
         </div>
+        <button id="btn-profile-save">Save</button>
       </form>
     </div>
   `;
@@ -140,10 +169,94 @@ function renderLogin(isSignUp) {
 
 function renderProfile(user) {
   contentContainer.innerHTML = buildProfileComponent(user);
+  document.getElementById("btn-change-avatar").onclick = onChangeAvatar;
+  document.getElementById("btn-profile-set-pass").onclick = onSetPassword;
+  document.getElementById("btn-profile-save").onclick = onSaveProfile;
+}
+
+function onChangeAvatar(event) {
+  event.preventDefault();
+  console.log("change avatar");
+}
+
+function onSetPassword(event) {
+  event.preventDefault();
+  console.log("set password");
+
+  const containerSetPass = document.getElementById("profile-set-pass");
+  containerSetPass.innerHTML = buildSetPassComponent();
+  document.getElementById("btn-save-pass").onclick = onSaveNewPassword;
+}
+
+function buildSetPassComponent() {
+  return `
+    <div class="set-password">
+      <div>
+        <label for="pass">New Password</label>
+        <input type="password" id="pass" autocomplete="off" />
+      </div>
+      <div>
+        <label for="confirm-pass">Confirm</label>
+        <input type="password" id="confirm-pass" autocomplete="off" />
+      </div>
+      <button id="btn-save-pass">Save Password</button>
+    </div>
+  `;
+}
+
+async function onSaveNewPassword(event) {
+  event.preventDefault();
+  const user = Moralis.User.current();
+
+  try {
+    // make sure new and confirmed password the same
+    const newPass = document.getElementById("pass").value;
+    const confirmPass = document.getElementById("confirm-pass").value;
+
+    if (newPass !== confirmPass) {
+      alert("passwords not equal");
+      return;
+    }
+
+    user.setPassword(newPass);
+    await user.save();
+    alert("Password updated successfully!");
+  } catch (error) {
+    console.error(error);
+    alert("Error while saving new password. See the console");
+  }
+}
+
+async function onSaveProfile(event) {
+  event.preventDefault();
+  const user = Moralis.User.current();
+
+  try {
+    // get values from the form
+    const username = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    // console.log("username:", username, "email:", email);
+
+    // update user object
+    user.setEmail(email);
+    user.setUsername(username);
+
+    await user.save();
+    alert("saved successfully!");
+  } catch (error) {
+    console.error(error);
+    alert("Error while saving. See the console.");
+  }
 }
 
 function render() {
+  const user = Moralis.User.current();
   renderHeader();
+  if (user) {
+    renderProfile(user);
+  } else {
+    renderWelcome();
+  }
 }
 
 // render on page load
