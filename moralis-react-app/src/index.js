a22a582524df26ecab3258b35cd695a2b9a4605f
ryan <rphansen91@gmail.com>
2021-03-16 19:07:34 -0700

    Format

--- moralis-react-app/src/index.js
+++ moralis-react-app/src/index.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import moralis from "moralis";
 
-moralis.initialize(process.env.REACT_APP_MORALIS_APPLICATION_ID)
-moralis.serverURL = process.env.REACT_APP_MORALIS_SERVER_URL
+moralis.initialize(process.env.REACT_APP_MORALIS_APPLICATION_ID);
+moralis.serverURL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
 const App = () => {
-  const [user, setUser] = useState(moralis.User.current())
+  const [user, setUser] = useState(moralis.User.current());
   const onLogin = async () => {
-    const user = await moralis.authenticate()
-    setUser(user)
-  }
+    const user = await moralis.authenticate();
+    setUser(user);
+  };
   const onLogout = () => {
-    moralis.User.logOut()
-    setUser(null)
-  }
+    moralis.User.logOut();
+    setUser(null);
+  };
   if (user) {
-    return <button onClick={onLogout}>Logout</button>
+    return <button onClick={onLogout}>Logout</button>;
   }
-  return <button onClick={onLogin}>Login</button>
-}
+  return <button onClick={onLogin}>Login</button>;
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
