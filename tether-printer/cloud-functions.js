21e1f2c8f85bb7be96a7c216e62641566ebe24ec
Thaddeus19 <51385192+Thaddeus19@users.noreply.github.com>
2021-10-05 23:06:29 -0500

    update the cloud function
    The ABI is added and the cloud function is corrected by changing data to amount and printing the value correctly.

--- tether-printer/cloud-functions.js
+++ tether-printer/cloud-functions.js
@@ -1,20 +1,33 @@
 // @@@ Plugins - enter this info into a new "RealTime Events" plugin @@@
 // description : Tether Issue
 // topic : Issue(uint256)
+/* abi : {
+  "anonymous": false,
+  "inputs": [
+    {
+      "indexed": false,
+      "name": "amount",
+      "type": "uint256"
+    }
+  ],
+  "name": "Issue",
+  "type": "event"
+}*/
 // address : 0xdac17f958d2ee523a2206206994597c13d831ec7
 // tableName : IssueEvent
-
 // @@@ Paste this code into the Moralis Cloud Functions section on the server @@@
 
 Moralis.Cloud.define("getIssueEvents", async function(request) {
-  const query = new Parse.Query("IssueEvent");
-  query.select("transaction_hash", "block_timestamp", "data");
+  const logger = Moralis.Cloud.getLogger();
+  const query = new Parse.Query("IssueEventTether");
+  query.select("transaction_hash", "block_timestamp", "amount");
   query.descending("block_timestamp");
   query.limit(10);
   const results = await query.find({ useMasterKey: true });
   return results.map(function(result) {
     // amount is an interger so must divide by the number of decimals in Tether (6)
-    const amount = Moralis.web3.utils.hexToNumber(result.attributes.data) / 1e6;
+    const amount = result.attributes.amount / 1e6;
+   logger.info(amount);
     return {
       transaction_hash: result.attributes.transaction_hash,
       block_timestamp: result.attributes.block_timestamp,
