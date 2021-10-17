3dadce6545a0205ad352aec149478352b69ab018
Attila Csanyi <attila.csanyi@shields.energy>
2021-10-17 20:44:50 +0200

    fix: default angular test

--- moralis-angular-app/src/app/app.component.ts
+++ moralis-angular-app/src/app/app.component.ts
@@ -10,6 +10,7 @@ Moralis.serverURL = 'https://cjqaddie5gbb.grandmoralis.com:2053/server';
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
       <h1>Welcome to {{ title }}!</h1>
+      <span style="display: block">{{ title }} app is running!</span>
       <img
         width="300"
         alt="Angular Logo"
