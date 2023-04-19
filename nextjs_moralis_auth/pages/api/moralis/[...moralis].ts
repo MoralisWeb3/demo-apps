7f8eb98e3cb0f270382ea01d6f0d1342f035f3ca
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-04-19 09:17:16 +0700

    🔒️ MoralisNextApi domain fix

--- nextjs_moralis_auth/pages/api/moralis/[...moralis].ts
+++ nextjs_moralis_auth/pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
     apiKey: process.env.MORALIS_API_KEY,
     authentication: {
-        domain: 'amazing.dapp',
+        domain: process.env.NEXTAUTH_URL ? new URL(process.env.NEXTAUTH_URL).host : '',
         uri: process.env.NEXTAUTH_URL,
         timeout: 120,
     },
